76ad879582464c1a2899a7014028f3e2901d4422
문희원 <heewon3589@gmail.com>
2026-05-13 15:07:27 +0900

    클래스 따로 만듬

--- src/gee.java
+++ src/gee.java
@@ -1,19 +1,21 @@
 import javax.swing.JFrame;
 
-
+class MyFrame extends JFrame{
+    public MyFrame(){
+                setTitle("300x300 스윙 프로그램 만들기");
+                setSize(300, 300);
+                setVisible(true);
+                }
+}
 public class gee extends JFrame{
-        public gee(){
-        setTitle("300x300 스윙 프로그램 만들기");
-        setSize(300, 300);
-        setVisible(true);
-        }
     
     public static void main(String[] args) {
         
 
+
         
+        MyFrame frame = new MyFrame();
         
-        gee frame = new gee();
         
 
 
