02cb8e1d58b99c7d7ca45a4287a0037227fb69cd
문희원 <heewon3589@gmail.com>
2026-05-13 14:46:18 +0900

    스윙 공부

---
+++ src/gee.java
@@ -0,0 +1,21 @@
+import javax.swing.JFrame;
+
+
+public class gee extends JFrame{
+        public gee(){
+        setTitle("300x300 스윙 프로그램 만들기");
+        setSize(300, 300);
+        setVisible(true);
+        }
+    
+    public static void main(String[] args) {
+        
+
+        
+        
+        gee frame = new gee();
+        
+
+
+    }
+}
