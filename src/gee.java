734b01d1fdbe01a53afc335060dc99adb38dafe0
문희원 <heewon3589@gmail.com>
2026-05-13 15:32:17 +0900

    버튼 및 컬러 추가

--- src/gee.java
+++ src/gee.java
@@ -1,11 +1,22 @@
-import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.*;
 
 class MyFrame extends JFrame{
     public MyFrame(){
-                setTitle("300x300 스윙 프로그램 만들기");
-                setSize(300, 300);
-                setVisible(true);
-                }
+        setTitle("ContentPane과 JFrame예제");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        getContentPane().setBackground(Color.ORANGE);
+        getContentPane().setLayout(new FlowLayout());
+
+        add(new JButton("OK"));
+        add(new JButton("Cnacel"));
+        add(new JButton("Ignore"));
+        
+        setTitle("300x300 스윙 프로그램 만들기");
+        setSize(300, 300);
+        setVisible(true);
+        }
 }
 public class gee extends JFrame{
     
