080ba5630139dcfc3f43407b8e43c006cab86a0b
문희원 <heewon3589@gmail.com>
2026-05-13 16:19:58 +0900

    레이아웃 left

--- src/gee.java
+++ src/gee.java
@@ -6,29 +6,45 @@ class MyFrame extends JFrame{
         setTitle("ContentPane과 JFrame예제");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        getContentPane().setBackground(Color.ORANGE);
-        getContentPane().setLayout(new FlowLayout());
+        getContentPane().setBackground(Color.ORANGE); //JFrame 배경색 설정 (실제로는 contentpan 에 적용됌)
+        getContentPane().setLayout(new FlowLayout()); //
 
         add(new JButton("OK"));
         add(new JButton("Cnacel"));
         add(new JButton("Ignore"));
         
-        setTitle("300x300 스윙 프로그램 만들기");
+        // setTitle("300x300 스윙 프로그램 만들기");
         setSize(300, 300);
         setVisible(true);
         }
 }
-public class gee extends JFrame{
+
+
+class MyFrame2 extends JFrame{
+    public MyFrame2(){
+        setTitle("ContentPane과 JFrame예제");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Container contentPane = getContentPane();
+
+        contentPane.setLayout(new FlowLayout(FlowLayout.LEFT, 30, 40));
     
+        getContentPane().setBackground(Color.ORANGE); //JFrame 배경색 설정 (실제로는 contentpan 에 적용됌)
+        getContentPane().setLayout(new FlowLayout()); //
+
+        add(new JButton("OK"));
+        add(new JButton("Cnacel"));
+        add(new JButton("Ignore"));
+        
+        // setTitle("300x300 스윙 프로그램 만들기");
+        setSize(300, 300);
+        setVisible(true);
+        }
+}
+
+public class gee extends JFrame{
     public static void main(String[] args) {
+          
+        MyFrame2 frame = new MyFrame2();
         
-
-
-        
-        MyFrame frame = new MyFrame();
-        
-        
-
-
     }
 }
